48adf654cdea8a8f3a066164ad11e1f0116c3b11
farll <farll@DESKTOP-P8D2H66>
2016-11-12 21:30:02 -0200

    Add project files.

---
+++ TGP_Web_Test_AI.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TGP_Web_Test_AI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
